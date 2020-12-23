1a513c3a8afc5573e39621082ed48a09dbce813b
Alejandro Murawczik <amurawczik@almacorp.es>
2020-12-23 19:34:32 +0100

    ADD navigation components

--- App.js
+++ App.js
@@ -1,15 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createAppContainer } from 'react-navigation'; 
+import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
+
+const Logo = () => {
+  return (
+    <Text>LALALA</Text>
+  )
+}
+
+
+const HomeScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Ir a detalle"
+        onPress={() => navigation.navigate('Detalle', {id: '2'})}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
+
+HomeScreen.navigationOptions = {
+  headerTitle: () => <Logo/>,
+}
+
+
+
+
+
+const DetalleScreen = ({navigation}) => {
+  const lala = navigation.getParam('lala', 'valor por defecto')
+  return (
+    <View style={styles.container}>
+      <Text>Soy la pantalla de detalle {lala}</Text>
+      <Button title="Volver"
+        onPress={() => navigation.setParams({ title: 'Usuario 1'})}
+      />
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+DetalleScreen.navigationOptions = ({ navigation, navigationOptions }) => {
+  return {
+    title: navigation.getParam('title', 'Cargando...'),
+    headerStyle: {
+      backgroundColor: navigationOptions.headerStyle.backgroundColor
+    }
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: HomeScreen
+  },
+  Detalle: {
+    screen: DetalleScreen
+  }
+}, { initialRouteName: 'Home',
+  defaultNavigationOptions: {
+    headerStyle: {
+    backgroundColor: '#fec'
+  },
+  headerTintColor: '#222',
+  headerTitleStyle: {
+    fontWeight: '100',
+    letterSpacing: 5
+  }
+  }
+})
+
+export default createAppContainer(AppNavigator)
 
 const styles = StyleSheet.create({
   container: {
